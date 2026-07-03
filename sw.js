3b43128ac30971541273602cadd1db45e982f781
Gustavo Perez <ing.gustavoperez@gmail.com>
2026-07-03 11:33:13 +0200

    Bump service worker version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── Service Worker · Aurum BC Security ──
-const VERSION = 'v20260702g';
+const VERSION = 'v20260702h';
 const CACHE   = 'aurum-' + VERSION;
 
 // Al instalar: guarda el HTML en caché
